772fe78480a405efb5a45488927b5a0e2ca1aa57
reggietenk <reggietenkorang@gmail.com>
2022-01-02 17:36:12 -0500

    fixed generate password function

--- script.js
+++ script.js
@@ -1,52 +1,117 @@
-let arr = []
 
-
-window.alert("Generate New Password")
-function generatePassword() {
-
-let upperCase =["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"]
-let lowerCase =["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
-let symbol = ['~,`,!,@,#,$,%,^,&,*,(,),_,-,+,=,{,[,},],]']
-let number =  ['0','1','2','3','4','5','6','7','8','9']
-
-
-let allChar = [...upperCase, ...lowerCase, ...symbol, ...number];
-console.log(allChar.length)
-
-
-for (let i = 0; i < allChar.length; i++) {
-   
-  let select = Math.floor(Math.random() * allChar.length);
-  arr.push(allChar[select])
-  }
-  arr.length = 12
-passwordText.value = password;
-console.log(arr.join())
-  
-}
-console.log(generatePassword)
-// Get references to the #generate element
+// Assignment Code
 let generateBtn = document.querySelector("#generate");
-console.log(generateBtn)
+let password = document.querySelector("#password")
 
+prompt("Please select password criteria")
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword); {
-  console.log(generatePassword)
+let generatePassword = function (){
+
+let allChar = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let passwordLength = 8-128;
+let password = "";
+for (let i = 0; i < passwordLength; i++) {
+  let randomNumber = Math.floor(Math.random() * allChar.length);
+password += allChar.substring(randomNumber,randomNumber+1);
+
+  document.getElementById("password").value = password
+  }
 
 }
+ 
+
+generateBtn.addEventListener("click", generatePassword);
 
 
-function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-  
-  
-  
-  
-  
-  passwordText.innerText = arr;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const password = prompt('please generate new password')
+
+// function generatePassword() {
+
+// let allChar = [...upperCase, ...lowerCase, ...symbol, ...number];
+// console.log(allChar.length)
+
+// let upperCase =["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"]
+// let lowerCase =["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
+// let symbol = ['~,`,!,@,#,$,%,^,&,*,(,),_,-,+,=,{,[,},],]']
+// let number =  ['0','1','2','3','4','5','6','7','8','9']
+
+// for (let i = 0; i < allChar.length; i++) {
    
+//   generatePassword
+// // }
+// // }
+
+// // Get references to the #generate element
+// let generateBtn = document.querySelector("#generate");
+// console.log(generateBtn)
+
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", generatePassword); {
+  
+
+// }
+
+
+// function writePassword() {
+//   let password = generatePassword();
+//   let passwordText = document.querySelector("#password");
+  
+  
+//   passwordText.innerHTML = generatePassword()
+//   console.log(writePassword)
   
   
   
@@ -54,4 +119,5 @@ function writePassword() {
 
 
 
-}
+// }
+
